99e39c787aff8a26ce0b75b428e384d0b23cfe3d
lasiimov <tiago2001ambrosio@gmail.com>
2025-09-11 14:42:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -109,4 +109,16 @@ enhanceBtn.addEventListener('click', async () => {
     loading.style.display = 'none';
     enhanceBtn.disabled = false;
   }
-});
+}
+               document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+});           
+                           
+                           
+                           );
